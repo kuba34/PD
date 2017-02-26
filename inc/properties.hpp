9623176092b4b11d30ac2047bdd707a29ed844fc
Jakub Głąb <jakubglab96@gmail.com>
2017-02-26 13:40:27 +0100

    Added CAN module
    Not working, yet.

--- inc/properties.hpp
+++ inc/properties.hpp
@@ -3,13 +3,14 @@ class Property
 {
 	int _reg, _err, _last_err=0;
 	float _val, _Kp, _Kd;
+	char* _can_device_id;
 	
 	public:
-	Property(float val, float Kp, float Kd, bool pid = false);
+	Property(float val, float Kp, float Kd, char* can_device_id);
 	void change_err(int new_err);
 	void calc_reg();
 	float get_val() const {return _val;}
 	float get_val() {return _val;}
 	int get_reg() const {return _reg;}
-	void parse_msg(float val);
+	void parse_msg(int reg);
 };
--- src/properties.cpp
+++ src/properties.cpp
@@ -1,11 +1,11 @@
 #include "PD.hpp"
 
-
-Property::Property(float x, float Kp, float Kd) 
+Property::Property(float val, float Kp, float Kd, char* can_device_id) 
 {
-	_val=val;
-	_Kp=Kp;
-	_Kd=Kd;
+	_val = val;
+	_Kp = Kp;
+	_Kd = Kd;
+	_can_device_id = can_device_id
 }
 
 void Property::change_err(int new_err)
@@ -16,6 +16,13 @@ void Property::change_err(int new_err)
 void Property::calc_reg()
 {
 	_err = center - pos;
-	_reg=PD(_err,_Kp,_Kd);
+	_reg = PD(_err,_Kp,_Kd);
 	_last_err = _err;
+}
+
+void can_send(int reg)
+{
+	char* reg_c = (char*)
+	char* msg = _can_device_id + '#' + (char*)reg;
+	cansend(msg);
 }
